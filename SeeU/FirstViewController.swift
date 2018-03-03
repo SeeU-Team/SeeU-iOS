e95df23ca958205a5f6b60545103af4bcb6fc8a3
thomasfouan <thomasfouan@hotmail.fr>
2018-03-03 16:57:08 +0100

    Initial Commit

---
+++ SeeU/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SeeU
+//
+//  Created by Thomas Fouan on 03/03/2018.
+//  Copyright © 2018 SeeU. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
