11edfa1f5903db61356e70b0e3e0b219357f8947
thomasfouan <thomasfouan@hotmail.fr>
2018-03-11 19:45:57 +0100

    add horizontal scroll view

--- SeeU/FirstViewController.swift
+++ SeeU/FirstViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var teamCollection: UICollectionView!
+    
+    var teams: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0 ..< 10 {
+            teams.append("team " + "\(i)")
+        }
+        
+        self.teamCollection.delegate = self;
+        self.teamCollection.dataSource = self;
+        
+        self.teamCollection.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,17 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print(teams.count)
+        return teams.count;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: TeamCollectionViewCell = teamCollection.dequeueReusableCell(withReuseIdentifier: "teamCollectionViewCell", for: indexPath) as! TeamCollectionViewCell
+        
+        cell.textView?.text = teams[indexPath.row]
+        return cell;
+    }
+    
 }
 
---
+++ SeeU/TeamCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TeamCollectionViewCell.swift
+//  SeeU
+//
+//  Created by Thomas Fouan on 11/03/2018.
+//  Copyright © 2018 SeeU. All rights reserved.
+//
+
+import UIKit
+
+class TeamCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
