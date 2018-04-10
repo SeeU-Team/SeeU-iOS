3dd9762eea2fecd2e9cada0cbe149113bd807df1
Vincent Rossignol <vincent@MacBook-Air-de-Vincent.local>
2018-04-10 12:07:03 +0200

    first work

---
+++ SeeU/Gradient.swift
@@ -0,0 +1,16 @@
+//
+//  Gradient.swift
+//  SeeU
+//
+//  Created by Vincent Rossignol on 10/04/2018.
+//  Copyright © 2018 SeeU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Gradient {
+    static var startColor: UIColor = UIColor(displayP3Red: 23.0/255, green: 222.0/255, blue: 204.0/255, alpha: 1.0)
+    static var endColor: UIColor = UIColor(displayP3Red: 46.0/255, green: 104.0/255, blue: 225.0/255, alpha: 1.0)
+    static var gradient: CAGradientLayer = CAGradientLayer()
+}
--- SeeU/SecondViewController.swift
+++ SeeU/SecondViewController.swift
@@ -9,9 +9,26 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var myTeamCard: UIView!
+    
+    let startColor: UIColor = UIColor(displayP3Red: 23.0/255, green: 222.0/255, blue: 204.0/255, alpha: 1.0)
+    let endColor: UIColor = UIColor(displayP3Red: 46.0/255, green: 104.0/255, blue: 225.0/255, alpha: 1.0)
+    let gradient: CAGradientLayer = CAGradientLayer()
+    
+    func initGradient() {
+        gradient.frame = myTeamCard.frame
+        gradient.colors = [startColor.cgColor, endColor.cgColor]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+        gradient.cornerRadius = 6.0;
+        
+        myTeamCard.layer.insertSublayer(self.gradient, at: 0)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initGradient()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
