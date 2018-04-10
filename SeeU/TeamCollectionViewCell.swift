13df15fd87338b2490cac3ff461caea054eae708
Vincent Rossignol <vincent@MacBook-Air-de-Vincent.local>
2018-04-10 10:22:40 +0200

    Team collection modification

--- SeeU/TeamCollectionViewCell.swift
+++ SeeU/TeamCollectionViewCell.swift
@@ -13,8 +13,25 @@ class TeamCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var tags: UILabel!
     @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var teamUpButton: UIButton!
+    
+    let startColor: UIColor = UIColor(displayP3Red: 23.0/255, green: 222.0/255, blue: 204.0/255, alpha: 1.0)
+    let endColor: UIColor = UIColor(displayP3Red: 46.0/255, green: 104.0/255, blue: 225.0/255, alpha: 1.0)
+    let gradient: CAGradientLayer = CAGradientLayer()
+    
+    func initGradient() {
+        gradient.frame = teamUpButton.bounds
+        gradient.colors = [startColor.cgColor, endColor.cgColor]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+        gradient.cornerRadius = 6.0;
+        
+        teamUpButton.layer.insertSublayer(self.gradient, at: 0)
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        initGradient()
+        teamUpButton.layer.cornerRadius = 6.0;
     }
 }
