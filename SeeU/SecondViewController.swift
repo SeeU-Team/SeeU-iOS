0963bcce226c3087936980a543eeabdb2c5bf4e5
Vincent Rossignol <vincent@MacBook-Air-de-Vincent.local>
2018-05-07 16:17:17 +0200

    messages interface

--- SeeU/SecondViewController.swift
+++ SeeU/SecondViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
-    @IBOutlet weak var myTeamCard: UIView!
+    @IBOutlet weak var myTeamCard: UIStackView!
     
     let startColor: UIColor = UIColor(displayP3Red: 23.0/255, green: 222.0/255, blue: 204.0/255, alpha: 1.0)
     let endColor: UIColor = UIColor(displayP3Red: 46.0/255, green: 104.0/255, blue: 225.0/255, alpha: 1.0)
     let gradient: CAGradientLayer = CAGradientLayer()
     
     func initGradient() {
-        gradient.frame = myTeamCard.frame
+        gradient.frame = myTeamCard.bounds
         gradient.colors = [startColor.cgColor, endColor.cgColor]
         gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
         gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
@@ -28,7 +28,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        initGradient()
+        // initGradient()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
